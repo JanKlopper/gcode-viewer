27eb7a543c6f1360a9817147f8aae289b97a8012
janklopper <janklopper@cavia.(none)>
2013-03-31 15:46:43 +0200

    Add gcode variables Add gcode math preprocessor Add gcode g0 command, change opacities for milling as oposed to 3d printing Change some gui elements

--- web/gcode-model.js
+++ web/gcode-model.js
@@ -3,13 +3,12 @@ function createObjectFromGCode(gcode) {
   //    http://reprap.org/wiki/G-code
   //    http://en.wikipedia.org/wiki/G-code
   //    SprintRun source code
-	
+
   var lastLine = {x:0, y:0, z:0, e:0, f:0, extruding:false};
- 
  	var layers = [];
  	var layer = undefined;
  	var bbbox = { min: { x:100000,y:100000,z:100000 }, max: { x:-100000,y:-100000,z:-100000 } };
- 	
+
  	function newLayer(line) {
  		layer = { type: {}, layer: layers.count(), z: line.z, };
  		layers.push(layer);
@@ -28,7 +27,7 @@ function createObjectFromGCode(gcode) {
  				color: color,
  				segmentCount: 0,
  				material: new THREE.LineBasicMaterial({
-					  opacity:line.extruding ? 0.5 : 0.4,
+					  opacity:line.extruding ? 0.8 : 0.6,
 					  transparent: true,
 					  linewidth: 1,
 					  vertexColors: THREE.FaceColors }),
@@ -40,7 +39,7 @@ function createObjectFromGCode(gcode) {
  	function addSegment(p1, p2) {
 		var group = getLineGroup(p2);
 		var geometry = group.geometry;
-		
+
 		group.segmentCount++;
         geometry.vertices.push(new THREE.Vertex(
             new THREE.Vector3(p1.x, p1.y, p1.z)));
@@ -57,7 +56,8 @@ function createObjectFromGCode(gcode) {
 			bbbox.max.z = Math.max(bbbox.max.z, p2.z);
 		}
  	}
-  	var relative = false;
+
+  var relative = false;
 	function delta(v1, v2) {
 		return relative ? v2 : v2 - v1;
 	}
@@ -65,7 +65,7 @@ function createObjectFromGCode(gcode) {
 		return relative ? v1 + v2 : v2;
 	}
 
-  var parser = new GCodeParser({  	
+  var parser = new GCodeParser({
     G1: function(args, line) {
       // Example: G1 Z1.0 F3000
       //          G1 X99.9948 Y80.0611 Z15.0 F1500.0 E981.64869
@@ -84,12 +84,31 @@ function createObjectFromGCode(gcode) {
       };
       /* layer change detection is or made by watching Z, it's made by
          watching when we extrude at a new Z position */
-		if (delta(lastLine.e, newLine.e) > 0) {
-			newLine.extruding = delta(lastLine.e, newLine.e) > 0;
-			if (layer == undefined || newLine.z != layer.z)
-				newLayer(newLine);
-		}
-		addSegment(lastLine, newLine);
+		  if (delta(lastLine.e, newLine.e) > 0) {
+			  newLine.extruding = delta(lastLine.e, newLine.e) > 0;
+			  if (layer == undefined || newLine.z != layer.z)
+				  newLayer(newLine);
+		  }
+		  addSegment(lastLine, newLine);
+      lastLine = newLine;
+    },
+
+    G0: function(args, line){
+      var newLine = {
+        x: args.x !== undefined ? absolute(lastLine.x, args.x) : lastLine.x,
+        y: args.y !== undefined ? absolute(lastLine.y, args.y) : lastLine.y,
+        z: args.z !== undefined ? absolute(lastLine.z, args.z) : lastLine.z,
+        e: args.e !== undefined ? absolute(lastLine.e, args.e) : lastLine.e,
+        f: args.f !== undefined ? absolute(lastLine.f, args.f) : lastLine.f,
+      };
+      /* layer change detection is or made by watching Z, it's made by
+         watching when we extrude at a new Z position */
+		  if (delta(lastLine.e, newLine.e) > 0) {
+			  newLine.extruding = delta(lastLine.e, newLine.e) > 0;
+			  if (layer == undefined || newLine.z != layer.z)
+				  newLayer(newLine);
+		  }
+		  addSegment(lastLine, newLine);
       lastLine = newLine;
     },
 
@@ -165,7 +184,7 @@ function createObjectFromGCode(gcode) {
 	console.log("Layer Count ", layers.count());
 
   var object = new THREE.Object3D();
-	
+
 	for (var lid in layers) {
 		var layer = layers[lid];
 //		console.log("Layer ", layer.layer);
@@ -185,7 +204,7 @@ function createObjectFromGCode(gcode) {
   		bbbox.min.y + ((bbbox.max.y - bbbox.min.y) / 2),
   		bbbox.min.z + ((bbbox.max.z - bbbox.min.z) / 2));
 	console.log("center ", center);
-  
+
   object.position = center.multiplyScalar(-scale);
 
   object.scale.multiplyScalar(scale);
--- web/gcode-parser.js
+++ web/gcode-parser.js
@@ -7,25 +7,65 @@
  */
 function GCodeParser(handlers) {
   this.handlers = handlers || {};
+  this.vars = {};
 }
+
+GCodeParser.prototype.replaceVars = function(output){
+  $('#parsedgcode').show();
+  for(varname in this.vars){
+    output = output.replace('#'+varname,
+                            this.vars[varname]);
+  }
+  return output;
+}
+
+GCodeParser.prototype.evalMath = function(input){
+  var output = '';
+  while(input.indexOf('[') > -1){
+    // concat every bit of text outside the brackets to the output
+    output = output + input.substr(0, input.indexOf('['));
+    var mathPart = input.substring(input.indexOf('[')+1, input.indexOf(']'))
+    // remove any non digits / Math operators, eval the result
+    output = output + eval(mathPart.replace(/[^\d+\+\*\/-\\.]/g, ''));
+    input = input.substr(input.indexOf(']')+1);
+  }
+  output = output + input;
+  return output;
+}
+
 
 GCodeParser.prototype.parseLine = function(text, info) {
   text = text.replace(/;.*$/, '').trim(); // Remove comments
   if (text) {
-    var tokens = text.split(' ');
-    if (tokens) {
-      var cmd = tokens[0];
-      var args = {
-        'cmd': cmd
-      };
-      tokens.splice(1).forEach(function(token) {
-        var key = token[0].toLowerCase();
-        var value = parseFloat(token.substring(1));
-        args[key] = value;
-      });
-      var handler = this.handlers[tokens[0]] || this.handlers['default'];
-      if (handler) {
-        return handler(args, info);
+    if(text[0] == '#'){
+        var parts = text.split('=');
+        var varname = parts[0].trim().substr(1);
+        this.vars[varname] = parseFloat(parts[1].trim(), 10);
+    } else {
+      if(text.indexOf('#') > -1){
+			  text = this.replaceVars(text);
+			}
+
+      if(text.indexOf('[') > -1 && text.indexOf(']') > -1){
+			  text = this.evalMath(text);
+			}
+
+      var tokens = text.split(' ');
+      if (tokens) {
+        var cmd = tokens[0];
+
+        var args = {
+          'cmd': cmd
+        };
+        tokens.splice(1).forEach(function(token) {
+          var key = token[0].toLowerCase();
+          var value = parseFloat(token.substring(1));
+          args[key] = value;
+        });
+        var handler = this.handlers[tokens[0]] || this.handlers['default'];
+        if (handler) {
+          return handler(args, info);
+        }
       }
     }
   }
